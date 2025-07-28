4a0876faaf405805d8c9725c62c2c567794700e4
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-28 16:59:39 -0700

    Complete 14_fibonacci javascript exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+let memo = [];
+memo[0] = 0;
+memo[1] = 1;
+memo[2] = 1;
 
+const fibonacci = function (number) {
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number > memo.length - 1) {
+        let oneBack = fibonacci(number - 1);
+        let twoBack = fibonacci(number - 2);
+        memo[number] = oneBack + twoBack;
+    }
+    return memo[number];
 };
 
 // Do not edit below this line
