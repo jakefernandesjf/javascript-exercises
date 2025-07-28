dc79347d65497e12057515ac3db01ea03062f2db
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-28 16:40:58 -0700

    Complete 13_palindromes javascript exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const PUNCTUATIONS_AND_SPACE = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+const palindromes = function (string) {
+    let left = 0;
+    let right = string.length - 1;
+    while (left < right) {
+        let charLeft = string
+            .charAt(left)
+            .toLowerCase();
+        let charRight = string
+            .charAt(right)
+            .toLowerCase();
+        if (isPunctuationOrSpace(charLeft)) {
+            left++;
+            continue;
+        }
+        if (isPunctuationOrSpace(charRight)) {
+            right--;
+            continue;
+        }
+
+        if (charLeft != charRight) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 };
 
+function isPunctuationOrSpace (char) {
+    return PUNCTUATIONS_AND_SPACE.includes(char);
+}
 // Do not edit below this line
 module.exports = palindromes;
