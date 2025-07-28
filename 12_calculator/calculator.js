72b9fe2bbef4cfe5f6f2d9f0fc4ef16358d75b4e
jakefernandesjf <46454692+jakefernandesjf@users.noreply.github.com>
2025-07-28 13:07:31 -0700

    Complete 12_calculator javascript exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, curr) => (total += curr), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  for (val of array) {
+    result *= val;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (value) {
+  let result = 1;
+  for (let i = value; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
